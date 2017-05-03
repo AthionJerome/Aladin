18609414c5064abce3fcdafe9408fa9f0a364d20
AthionJerome <jerome.athion@orange.fr>
2017-05-03 15:35:04 +0400

    Add files via upload

---
+++ Controleur/controleur (2).php
@@ -0,0 +1,47 @@
+<?php
+	function accueil()
+	{
+		require './vue/vueAccueil.php';
+	}
+	function produits()
+	{
+		require './vue/vueProduits.php';
+	}
+	function clients()
+	{
+		require './vue/vueClients.php';
+	}
+	function inscription()
+	{
+		require '.vue/vueInscription.php';
+	}
+	function devis()
+	{
+		require './vue/vueDevis.php';
+	}
+	function facture()
+	{
+		require '.vue/vueFacture.php';
+	}
+	function systeme()
+	{
+		require '.vue/vueSyst&eacute;me.php';
+	}
+	function specifiques()
+	{
+		require '.vue/vueSp&eacute;cifiques.php';
+	}
+	function profil()
+	{
+		require '.vue/vueProfil.php';
+	}
+	function changeuser()
+	{
+		require '.vue/vueChangeUser.php';
+	}
+	function save()
+	{
+		require '.vue/vueSaveDataBase.php';
+	}
+
+?>
